701ad7a1c3389b0e16cf555f8aa5d11f696f2d95
KIM A RAN <angelless00@naver.com>
2024-11-14 15:16:27 +0900

    autolabeler ver.2

--- autolabel_utils.py
+++ autolabel_utils.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import os
 import cv2
 
-def auto_laveler_IMG(IMAGE_PATH):
+def auto_laveler_IMG(IMAGE_PATH,model):
     """IMAGE 폴더안에 있는 모든 img 라벨링 후 라벨 텍스트 저장"""
     image_list=os.listdir(IMAGE_PATH)
     for image in image_list:
@@ -15,7 +15,7 @@ def video_capture(video_path,image_path,second):
     video 캡쳐  
     second=몇초마다캡쳐할건지
     """
-    video = cv2.VideoCapture(video_path)
+    video = cv2.VideoCapture(video_path,model)
     if not os.path.exists(image_path):
          os.mkdir(image_path)
     num_frame=video.get(cv2.CAP_PROP_FPS)   # 현재 파일의 FPS 
@@ -29,7 +29,7 @@ def video_capture(video_path,image_path,second):
         video.set(cv2.CAP_PROP_POS_FRAMES, int(i*frame_step))
         i+=1
 
-def detect_save(IMAGE_PATH):
+def detect_save(IMAGE_PATH,model):
     """IMAGE_PATH안에 있는 파일중 detect 된 파일과 라벨만 folder 안에 저장"""
     image_list=os.listdir(IMAGE_PATH)
     for image in image_list:
